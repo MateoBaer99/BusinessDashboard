eb02054ae95499774d7f88ff9898b1a551c4ec1e
Mateo <mateobaer38@hotmail.com>
2026-04-10 19:12:58 +0200

    started charts.py

--- src/business_dashboard/charts.py
+++ src/business_dashboard/charts.py
@@ -1,3 +1,9 @@
 """
 `Pandas/plotly chart generation module (pure functions: dataframe in -> go.Figure out, or HTML fragment out)
 """
+from transactions_loader import load_transactions_csv
+
+DF = load_transactions_csv()
+
+if __name__ == "__main__" :
+    print(DF)
